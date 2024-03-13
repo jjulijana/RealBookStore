c62490d52077cb4873bb892a8c754c25702ec750
Julijana <mi20025@alas.matf.bg.ac.rs>
2024-03-13 23:44:15 +0100

    SQL injection i Cross-site scripting attack
    Added comment that explains sqli i xss attacks that can be used against our app

--- src/main/java/com/urosdragojevic/realbookstore/repository/CommentRepository.java
+++ src/main/java/com/urosdragojevic/realbookstore/repository/CommentRepository.java
@@ -28,6 +28,21 @@ public class CommentRepository {
     public void create(Comment comment) {
         String query = "insert into comments(bookId, userId, comment) values (" + comment.getBookId() + ", " + comment.getUserId() + ", '" + comment.getComment() + "')";
 
+        // When a user submits a comment, the comment is inserted into the database without any validation or sanitization.
+        // This allows for SQL injection and XSS attacks.
+        // Test the application by submitting the following comments on http://localhost:8080/books/{id}:
+
+
+        // SQL injection
+        // We can see that the query is constructed by concatenating strings, which is a bad practice.
+        // komentar'); insert into persons(firstName, lastName, email) values ('Julijana', 'Jevtic', 'julijana@gmail.com'); --
+
+        // SQL injection & XSS
+        // Xss attack is happening when we search for a user which we're adding to the database.
+        // Picture source doesn't exist, but onerror is triggered and we get an alert with the cookie.
+        // komentar'); insert into persons(firstName, lastName, email) values ('Julijana', 'Jevtic', '<img src="neki.jpg" onerror="alert(document.cookie);">'); --
+
+
         try (Connection connection = dataSource.getConnection();
              Statement statement = connection.createStatement();
         ) {
